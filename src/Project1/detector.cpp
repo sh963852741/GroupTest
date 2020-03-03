d6c85777a3ea2d9a94bf70572accd9014fabc24b
宋润涵 <53166321+sh963852741@users.noreply.github.com>
2020-03-03 11:24:49 +0800

    可以检测对齐点了
    修复之前数据块个数参数无效的问题

--- src/Project1/detector.cpp
+++ src/Project1/detector.cpp
@@ -1,11 +1,13 @@
 #include "detector.h"
-using namespace std;
+
 Detector::Detector(Mat image) :image(image) {};
 int Detector::Detect()
 {
-	FinderPatternFinder finder;
-	finderPatternInfo = finder.FindFinderPattern(image);
-	Point2d point = CalcPosition(25, 25, 3, 3);
+	finderPatternInfo = finder1.FindFinderPattern(image);
+	double moduleSize = CalculateModuleSize();
+	double estAlignmentX = finderPatternInfo.topRight.x - finderPatternInfo.topLeft.x + finderPatternInfo.bottomLeft.x;
+	double estAlignmentY = finderPatternInfo.topRight.y - finderPatternInfo.topLeft.y + finderPatternInfo.bottomLeft.y;
+	alignmentPattern = FindAlignmentInRegion(moduleSize, estAlignmentX, estAlignmentY, 4);
 	return ProcessFinderPatternInfo(finderPatternInfo);
 }
 
@@ -20,13 +22,20 @@ unsigned short** Detector::GetBinaryData(int width, int height)
 	{
 		for (int j = 0; j < width; ++j)
 		{
-			Point2d point = CalcPosition(25, 25, j, i);
+			Point2d point = CalcPosition(width, height, j, i);
 			res[i][j] = image.at<uchar>(point) ? 1 : 0;
-			cout << res[i][j] << ' ';
+			cout << res[i][j];
 		}
 		cout << '\n';
 	}
 	return res;
+}
+
+double Detector::CalculateModuleSize()
+{
+	double size1 = (finderPatternInfo.topRight.x - finderPatternInfo.topLeft.x) / (192 - 7);
+	double size2 = (finderPatternInfo.bottomLeft.y-finderPatternInfo.topLeft.y) / (108 - 7);
+	return (size1 + size2) / 2.0;
 }
 
 int Detector::ProcessFinderPatternInfo(FinderPatternInfo info)
@@ -42,4 +51,20 @@ Point2d Detector::CalcPosition(int width,int height, int x, int y)
 	dy = (finderPatternInfo.bottomLeft.y - finderPatternInfo.topLeft.y) / (height - 7);
 
 	return Point2d(finderPatternInfo.topLeft.x + (x - 3) * dx, finderPatternInfo.topLeft.y + (y - 3) * dy);
+}
+
+AlignmentPattern Detector::FindAlignmentInRegion(double overallEstModuleSize, int estAlignmentX, int estAlignmentY, int allowanceFactor)
+{
+	int allowance = cvFloor(allowanceFactor * overallEstModuleSize);
+	int alignmentAreaLeftX = max(0, estAlignmentX - allowance);
+	int alignmentAreaRightX = min(image.cols - 1, estAlignmentX + allowance);
+	if (alignmentAreaRightX - alignmentAreaLeftX < overallEstModuleSize * 3)
+	{
+		throw "Error";
+	}
+
+	int alignmentAreaTopY = max(0, estAlignmentY - allowance);
+	int alignmentAreaBottomY = min(image.rows - 1, estAlignmentY + allowance);
+
+	return alignmentFinder.Find(image, alignmentAreaLeftX, alignmentAreaTopY, alignmentAreaRightX - alignmentAreaLeftX, alignmentAreaBottomY - alignmentAreaTopY, overallEstModuleSize);
 }
